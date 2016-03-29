5e12ae4a7acd82ec8887cd3b72a04d04e09764dd
mxmo0rhuhn <code@schrimpf.ch>
2016-03-29 12:21:17 +0200

    image optimization for better load times

--- public/image.php
+++ public/image.php
@@ -5,7 +5,7 @@ if(isset($_GET['src'])){
         $width = ceil($_GET['width'] / 10) * 10;
         $out = 'w'.$width.'_'.basename($_GET['src']);
         if(!is_file('cache/'.$out)){
-            exec('convert .'.$_GET['src'].' -resize '.$width.' cache/'.$out );
+            exec('convert .'.$_GET['src'].' -interlace Plane -strip  -quality 70 -resize '.$width.' cache/'.$out );
         }
         echo '/cache/'.$out;
     }else{
