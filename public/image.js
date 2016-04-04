8d01d101488a75565659326c3cf98f918d506e0b
Philipp Gamper <phgamper@phgamper.ch>
2016-04-04 12:19:34 +0200

    queued ajax calls

--- public/image.js
+++ public/image.js
@@ -1,19 +1,30 @@
-$(".img-resize").each(function(){
-    var div = $(this);
-    var items  = div.find("img");
-    if( items.length  > 0 ){
-        var img = items.first(); // get id of first image
+function enqueue(qname, div) {
+    $(document).queue(qname, function() {
+        var items  = div.find("img");
+        if( items.length  > 0 ){
+            var img = items.first(); // get id of first image
 
-        $.ajax({
-            type: 'GET',
-            url: '/image.php?src=' + img.data('src') + '&width=' + div.context.offsetWidth,
-            data: 'html',
-            success: function(data){
-                img.attr('src', data);
-            }
-        });
-    } else {
-        // images not found
-    }
-});
+            $.ajax({
+                type: 'GET',
+                url: '/image.php?src=' + img.data('src') + '&width=' + div.context.offsetWidth,
+                data: 'html',
+                success: function(data){
+                    img.attr('src', data);
+                    $(document).dequeue(qname);
+                }
+            });
+        } else {
+            // images not found
+            $(document).dequeue(qname);
+        }
+    });
+}
+
+$(document).ready(function() {
+    var queue = 'queue';
+    $(".img-resize").each(function(){
+        enqueue(queue, $(this));
+    });
+    $(document).dequeue(queue);
+})
 
