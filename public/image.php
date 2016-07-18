3f0dbb45818cce85790728be7c526a85305b4693
Philipp Gamper <phgamper@phgamper.ch>
2016-07-18 19:10:48 +0200

    check for width being in a certain range and src being an existing file, issue #38

--- public/image.php
+++ public/image.php
@@ -1,16 +1,20 @@
 <?php
 
-if(isset($_GET['src'])){
-    if(isset($_GET['width'])) {
+if (isset($_GET['src'])) {
+    if (isset($_GET['width'])) {
         $src = $_GET['src'];
         $width = $_GET['width'];
         $width = ceil($width / 10) * 10;
-        $out = 'cache/w'.$width.'_'.basename($src);
-        if(!is_file($out)){
-            exec('convert .'.escapeshellarg($src).' -interlace Plane -strip  -quality 70 -resize '.escapeshellarg($width).' '.escapeshellarg($out));
+        $out = 'cache/w' . $width . '_' . basename($src);
+        if (!(is_numeric($width) && $width > 0 && $width < 4000 && is_file(getcwd().$src))) {
+            echo $_GET['src'];
+            exit;
         }
-        echo '/'.$out;
-    }else{
+        if (!is_file($out)) {
+            exec('convert .' . escapeshellarg($src) . ' -interlace Plane -strip  -quality 70 -resize ' . escapeshellarg($width) . ' ' . escapeshellarg($out));
+        }
+        echo '/' . $out;
+    } else {
         echo $_GET['src'];
     }
 }
